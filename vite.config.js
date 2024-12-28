8bdf1a55c059e1e66e3ca87b6166687672ada86f
s-mathavan <s.mathavanjayalakshmi@gmail.com>
2024-12-28 21:37:15 +0530

    Added extra config for vite dev server.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],// Specify the custom port number here
+  strictPort: true, // Ensures the port isn't switched if unavailable
+  host: '0.0.0.0', // Optional: Allows access from external devices
+  open: true, // Optional: Automatically opens the browser
 })
